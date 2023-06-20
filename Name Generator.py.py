1bd38bc02c1352ef07e31c4efba008260c0fe531
gabrielexposito13 <ge1316a@american.edu>
2023-06-20 00:02:58 -0400

    Create Name Generator.py.py

---
+++ Name Generator.py.py
@@ -0,0 +1,8 @@
+#Band Name Generator 
+
+print("This is a book title generator.")
+city = input("What city were you born in?\n ")
+pet = input("What is the name of your pet?\n ")
+print("The name of your band is "+ city + " " + pet)
+input()
+
